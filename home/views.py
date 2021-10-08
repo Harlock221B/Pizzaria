d86cf6f27ff58d80f60b4090b67c62694ec0b845
Harlock221B <gabrielferreira221b@gmail.com>
2021-10-08 11:16:40 -0300

    commit fds

--- home/views.py
+++ home/views.py
@@ -4,7 +4,23 @@ from django.contrib import messages
 from .models import Cardapio
 
 def index(request):
-    
     return render(request,'home/index.html')
 
+def cardapio(request):
+    cardapios = Cardapio.objects.all()
+    return render(request,'home/cardapio.html',{'cardapios':cardapios})
+
+def buscar(request):
+    query = request.GET.get('q')
+    if query:
+        cardapios = Cardapio.objects.filter(
+            Q(nome__icontains=query) |
+            Q(descricao__icontains=query) |
+            Q(preco__icontains=query)
+        )
+    else:
+        cardapios = Cardapio.objects.all()
+    return render(request,'home/cardapio.html',{'cardapios':cardapios})
+
+def 
 # Create your views here.
